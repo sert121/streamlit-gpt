b3a1c00e034e1fd1c835de95af10415727d1d449
sert121 <yashrajmore29@gmail.com>
2023-04-28 17:19:26 +0530

    added custom prompt, https support

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from langchain.agents import initialize_agent, Tool
 from langchain.llms import OpenAI
 from langchain.chains.conversation.memory import ConversationBufferMemory
 import streamlit as st
-
+from urllib.parse import urlparse
 
 load_dotenv()
 openai.key = os.getenv("OPENAI_API_KEY")
 @st.cache_resource
-def generate_roadmap(urls):
+def generate_roadmap(urls, prompt_base):
     BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
 
     loader = BeautifulSoupWebReader()
@@ -43,12 +43,7 @@ def generate_roadmap(urls):
     # output = agent_chain.run(input="What is the Company about? What kind of services does it deal with?")
     
     prompt = f'''
-    Imagine you have a PhD in Machine learning, and a MBA as well. 
-    You understand product market fit really well, and also see the pain points when provided a company description and their workings.
-    You are also very well versed in generative ai technologies and have sucessfully founded 4 AI startups in the last few years. Given your breadth of experience, one you are given a company url, you need to first identify how generative ai can be useful for the company and then suggest a product roadmap incorporating your plans on integration. Be detailed yet concise, and don’t give generic advice.
-    Give highly professional company-specific advice on the roadmap.
-    Be detailed yet concise, and don’t give generic advice. 
-    Give highly professional company-specific advice on the roadmap.
+    {prompt_base}
 
     Company Website Description: 
     {output}
@@ -78,10 +73,59 @@ if __name__ == '__main__':
     # user_input = input("Enter a url: ")
     st.title("TribeGPT")
     user_input = st.text_input("Enter a url: ", key =999)
+    parsed_url = urlparse(user_input)
+
+    if parsed_url.scheme == '':
+        user_input = 'https://' + user_input
+
+    # write options
+    st.markdown('### Select a Prompt')
+    selected_prompt = st.selectbox(
+        'Select a Prompt',
+        ('Prompt #1', 'Prompt #2', 'Prompt #3', 'Custom Prompt')
+    )
+    if selected_prompt == 'Prompt #1':
+        prompt_base = '''
+        Imagine you have a PhD in Machine learning, and a MBA as well. 
+        You understand product market fit really well, and also see the pain points when provided a company description and their workings.
+        You are also very well versed in generative ai technologies and have sucessfully founded 4 AI startups in the last few years. Given your breadth of experience, one you are given a company url, you need to first identify how generative ai can be useful for the company and then suggest a product roadmap incorporating your plans on integration. Be detailed yet concise, and don’t give generic advice.
+        Give highly professional company-specific advice on the roadmap.
+        Be detailed yet concise, and don’t give generic advice. 
+        Give highly professional company-specific advice on the roadmap.
+        '''
+
+        st.write(prompt_base)
+
+    if selected_prompt == 'Prompt #2':
+        prompt_base = '''
+        You are professional with 10 years of experience in artificial intelligence. ]
+        You have spent time as an academic, research scientist, and have co-founded multiple startups in the AI space. 
+        You are a part of Tribe AI, who helps identify companies how they could integrate generative AI technologies.
+        As a representative of Tribe AI, understand their market and their blockers and
+        suggest them a comprehensive roadmap on how they could properly use generative AI for their benefit. 
+        Personalize the roadmap according to the company’s domain and website, don’t give useless suggestions.
+        '''
+        st.write(prompt_base)
+
+    if selected_prompt == 'Prompt #3':
+        prompt_base = '''
+        You are product professional with 10 years of experience in artificial intelligence. 
+        You have spent time as an academic, research scientist, and have co-founded multiple startups in the AI space.
+        You are a part of Tribe AI, who helps identify companies how they could integrate generative AI technologies.
+        You need to deeply understand their market and their blockers from the website info provided.
+        Suggest them a product roadmap on how they could properly use generative AI for their benefit.
+        Fit the product roadmap according to the company’s domain and website. 
+        Don’t give useless suggestions. DO NOT PROVIDE BASIC AND GENERIC SUGGESTIONS.        
+
+        '''
+        st.write(prompt_base)
+    if selected_prompt == 'Custom Prompt':
+        prompt_base = st.text_area("Enter a prompt: ", key = 998)
+        
 
     if st.button('Submit'):
         with st.spinner():
-            output, response = generate_roadmap([user_input])
+            output, response = generate_roadmap([user_input],prompt_base = prompt_base)
 
         if response is not None:
             st.balloons()
